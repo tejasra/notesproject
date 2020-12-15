655a4ec90d1b802a353220fdb8f24e7461663794
tejasra <56512902+tejasra@users.noreply.github.com>
2020-12-15 19:02:34 +0530

    Create add.js

---
+++ routes/add.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const client = require("./database");
+
+const checkSession = (req, res, next) => {
+  !req.session.user ? res.redirect("/login") : next();
+};
+
+router.get("/", checkSession, (req, res) => {
+  res.render("add");
+});
+
+router.post("/", (req, res) => {
+  !client._connected ? client.connect() : null;
+  client
+    .query("INSERT INTO NOTES(username,title,date,data) VALUES($1,$2,$3,$4)", [
+      req.session.user,
+      req.body.title,
+      new Date(),
+      req.body.description,
+    ])
+    .then(() => {
+      console.log("The Notes added...");
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.status(404).send("Error !!!!", err);
+    });
+});
+module.exports = router;
